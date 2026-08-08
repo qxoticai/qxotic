28f9729e0b63f54a91525ec0092b17b4af3fd346
Alfonso² Peterssen <a2peterssen@gmail.com>
2026-08-09 00:55:22 +0200

    drop deprecated/: fully phased out by jinfer
    Delete the phased-out scratch and span modules, their reactor profile,
    and the stale root logging.properties.

--- deprecated/scratch/src/main/java/com/qxotic/model/llm/HuggingFaceModelLoaderFactory.java
+++
@@ -1,8 +0,0 @@
-package com.qxotic.model.llm;
-
-public interface HuggingFaceModelLoaderFactory extends ModelLoaderFactory<Object> {
-    @Override
-    default String getFormatName() {
-        return "huggingface";
-    }
-}
