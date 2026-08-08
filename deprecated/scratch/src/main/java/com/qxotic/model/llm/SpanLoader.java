1e6b5ed792a7dd45473adccfd72ec667958d58c1
Alfonso² Peterssen <a2peterssen@gmail.com>
2026-08-09 00:55:22 +0200

    drop deprecated/: fully phased out by jinfer
    Delete the phased-out scratch and span modules, their reactor profile,
    and the stale root logging.properties.

--- deprecated/scratch/src/main/java/com/qxotic/model/llm/SpanLoader.java
+++
@@ -1,9 +0,0 @@
-package com.qxotic.model.llm;
-
-import com.qxotic.span.FloatSpan;
-import java.util.function.Function;
-
-public interface SpanLoader extends Function<Object, FloatSpan>, AutoCloseable {
-    @Override
-    default void close() throws Exception {}
-}
