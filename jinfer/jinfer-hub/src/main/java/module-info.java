b184b57e10db195f53914c51f6c01ed4a1b1db00
Alfonso² Peterssen <a2peterssen@gmail.com>
2026-08-25 11:10:41 +0200

    hub: add explicit JPMS module

---
+++ jinfer/jinfer-hub/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/** Model reference resolution and downloads. */
+module com.qxotic.jinfer.hub {
+    requires com.qxotic.format.json;
+    requires java.net.http;
+
+    exports com.qxotic.jinfer.hub;
+}
