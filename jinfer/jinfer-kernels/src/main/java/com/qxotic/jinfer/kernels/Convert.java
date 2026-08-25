467f9990b25e3af361216ca1b16397d2d322bd95
Alfonso² Peterssen <a2peterssen@gmail.com>
2026-08-25 13:48:03 +0200

    Minor formatting nit

--- jinfer/jinfer-kernels/src/main/java/com/qxotic/jinfer/kernels/Convert.java
+++ jinfer/jinfer-kernels/src/main/java/com/qxotic/jinfer/kernels/Convert.java
@@ -34,7 +34,8 @@ public final class Convert {
 
     /** Decode F_SPECIES.length() consecutive F16 values to an F32 vector (IEEE half -> single). */
     @AlwaysInline(
-            "hot Vector API helper: escaping FloatVector boxes per call (see hotspot_compile_commands)")
+            "hot Vector API helper: escaping FloatVector boxes per call (see"
+                    + " hotspot_compile_commands)")
     public static FloatVector f16ToF32Vector(MemorySegment memSeg, long byteOffset) {
         ShortVector bits16 =
                 ShortVector.fromMemorySegment(
